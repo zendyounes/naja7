ea0cd8abc9252af0071cd8ffa4a40392e18bf8a4
zendyounes <116553595+zendyounes@users.noreply.github.com>
2023-09-02 00:09:43 +0100

    Create countdown.js

---
+++ countdown.js
@@ -0,0 +1,21 @@
+// Set your countdown date here (format: "Month Day, Year HH:MM:SS")
+const countdownDate = new Date("September 30, 2023 00:00:00").getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+    
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+    if (distance < 0) {
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
